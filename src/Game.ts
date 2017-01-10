d3c1eea31268dc2302db26c72f316010dcdf94ac
Aristona <iamaristona@windowslive.com>
2017-01-10 13:55:19 +0300

    Continuing to convert everything into Typescript

---
+++ src/Game.ts
@@ -0,0 +1,26 @@
+/// <reference types="typescript" />
+/// <reference path="../typings/globals/socket.io-client/index.d.ts" />
+
+import * as Phaser from "phaser";
+import * as io from "socket.io-client";
+
+export class Game
+{
+    public game: Phaser.Game;
+
+    constructor() {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, "");
+    }
+
+    preload() {
+
+    }
+
+    start() {
+        console.log("boo");
+    }
+}
+
+const game = new Game;
+game.preload();
+game.start();
