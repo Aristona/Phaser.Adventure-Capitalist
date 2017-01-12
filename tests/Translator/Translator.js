daa68a66c467c2608c67667b1a5e31e9537761b3
Aristona <iamaristona@windowslive.com>
2017-01-12 19:03:21 +0300

    Tests should not be ignored.

---
+++ tests/Translator/Translator.js
@@ -0,0 +1,29 @@
+/* eslint-env mocha */
+
+const expect = require("chai").expect;
+
+describe("Suite", () => {
+
+    it("should work", () => expect(true).to.be.true);
+
+});
+
+describe("Translator", () => {
+
+    beforeEach((done) => {
+        done();
+    });
+
+    it("should return missing key if key is not found", () => {
+
+    });
+
+    it("should return key if key is found", () => {
+
+    });
+
+    it("should replace %s", () => {
+
+    });
+
+});
